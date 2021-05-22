4ac34ce3af168008d8e7f747cebb3cf2ff781cb5
Alys Scorpio <alyssad1182@gmail.com>
2021-05-22 15:19:46 -0700

    Adding starting variables to Javascript

---
+++ assets/java/script.js
@@ -0,0 +1,16 @@
+// Declare starting variables
+var startButton = document.querySelector('#start');
+var timer = document.querySelector('timer');
+var quizStart = document.querySelector('quiz-start');
+var quizQuestions = document.querySelector('quiz-questions');
+
+
+// when start button is clicked, timer starts, user is presented with a question
+
+// when question is answered, user is prompted with another question
+
+// when question is answered incorrectly time is subtracted from the timer
+
+// when all questions are answered or timer reaches 0, game over
+
+// when game is over, user is able to save initials and score
