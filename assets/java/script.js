3067755139beb5704c9fb661a660904e265dcc5c
Alys Scorpio <alyssad1182@gmail.com>
2021-05-22 16:09:38 -0700

    Adding quiz questions to JavaScript

--- assets/java/script.js
+++ assets/java/script.js
@@ -4,6 +4,26 @@ var timer = document.querySelector('timer');
 var quizStart = document.querySelector('quiz-start');
 var quizQuestions = document.querySelector('quiz-questions');
 
+var questions = [{
+    question: 'What HTML element defines a paragraph?',
+    answers: ['<paragraph>', '<p1>', '<p>', '<par>'],
+    correct: '<p>'
+},
+{
+    question: 'In CSS, what property specifies the font of a portion of text?',
+    answers: ['font', 'font-family', 'font-style', 'font-display'],
+    correct: 'font-family'
+},
+{
+    question: 'In JavaScript, what is the name for a special variable that holds more than one value? (Example: var names = ["John", "Paul", "Ringo"];)',
+    answers: ['String', 'Boolean', 'Loop', 'Array'],
+    correct: 'Array'
+},
+{
+    question: 'How can JavaScript access and change elements within an HTML document?',
+    answers: ['the DOM', 'jQuery', 'the Server', 'APIs'],
+    correct: 'the DOM'
+}];
 
 // when start button is clicked, timer starts, user is presented with a question
 
